45086f98629f425bad78d2f70974be028ada6cea
megonemad1 <rhys.puddephatt@btinternet.com>
2015-02-03 11:48:03 +0000

    socs Proxy

---
+++ ProxyGui/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace ProxyGui
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.TxtHostName = new System.Windows.Forms.TextBox();
+            this.TxtCientPort = new System.Windows.Forms.TextBox();
+            this.TxtHostPort = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TxtUserName = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.TxtPassword = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chkAdvanced = new System.Windows.Forms.CheckBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.ChkVerbose = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // TxtHostName
+            // 
+            this.TxtHostName.Location = new System.Drawing.Point(65, 5);
+            this.TxtHostName.Name = "TxtHostName";
+            this.TxtHostName.Size = new System.Drawing.Size(106, 20);
+            this.TxtHostName.TabIndex = 0;
+            // 
+            // TxtCientPort
+            // 
+            this.TxtCientPort.Location = new System.Drawing.Point(65, 51);
+            this.TxtCientPort.Name = "TxtCientPort";
+            this.TxtCientPort.Size = new System.Drawing.Size(106, 20);
+            this.TxtCientPort.TabIndex = 2;
+            this.TxtCientPort.Text = "8080";
+            // 
+            // TxtHostPort
+            // 
+            this.TxtHostPort.Location = new System.Drawing.Point(65, 28);
+            this.TxtHostPort.Name = "TxtHostPort";
+            this.TxtHostPort.Size = new System.Drawing.Size(106, 20);
+            this.TxtHostPort.TabIndex = 1;
+            this.TxtHostPort.Text = "22";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "HostName";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "HostPort";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "ClientPort";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(2, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "UserName";
+            // 
+            // TxtUserName
+            // 
+            this.TxtUserName.Location = new System.Drawing.Point(65, 73);
+            this.TxtUserName.Name = "TxtUserName";
+            this.TxtUserName.Size = new System.Drawing.Size(106, 20);
+            this.TxtUserName.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(2, 98);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Password";
+            // 
+            // TxtPassword
+            // 
+            this.TxtPassword.Location = new System.Drawing.Point(65, 95);
+            this.TxtPassword.Name = "TxtPassword";
+            this.TxtPassword.PasswordChar = '*';
+            this.TxtPassword.Size = new System.Drawing.Size(106, 20);
+            this.TxtPassword.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(5, 142);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.BtnStartStop_Click);
+            // 
+            // chkAdvanced
+            // 
+            this.chkAdvanced.AutoSize = true;
+            this.chkAdvanced.Location = new System.Drawing.Point(65, 121);
+            this.chkAdvanced.Name = "chkAdvanced";
+            this.chkAdvanced.Size = new System.Drawing.Size(15, 14);
+            this.chkAdvanced.TabIndex = 11;
+            this.chkAdvanced.UseVisualStyleBackColor = true;
+            this.chkAdvanced.CheckedChanged += new System.EventHandler(this.chkAdvanced_CheckedChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(3, 121);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Advanced";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(96, 121);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(46, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Verbose";
+            // 
+            // ChkVerbose
+            // 
+            this.ChkVerbose.AutoSize = true;
+            this.ChkVerbose.Location = new System.Drawing.Point(148, 121);
+            this.ChkVerbose.Name = "ChkVerbose";
+            this.ChkVerbose.Size = new System.Drawing.Size(15, 14);
+            this.ChkVerbose.TabIndex = 14;
+            this.ChkVerbose.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(175, 170);
+            this.Controls.Add(this.ChkVerbose);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.chkAdvanced);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.TxtPassword);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TxtUserName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TxtHostPort);
+            this.Controls.Add(this.TxtCientPort);
+            this.Controls.Add(this.TxtHostName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtHostName;
+        private System.Windows.Forms.TextBox TxtCientPort;
+        private System.Windows.Forms.TextBox TxtHostPort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TxtUserName;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox TxtPassword;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox chkAdvanced;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.CheckBox ChkVerbose;
+    }
+}
+
---
+++ ProxyGui/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ProxyLib;
+using System.IO;
+using System.Reflection;
+
+namespace ProxyGui
+{
+    public partial class Form1 : Form
+    {
+        Proxy _prox;
+        public Form1()
+        {
+            InitializeComponent();
+            string path = "ProxyLib.dll";
+            if (!File.Exists(path))
+                System.IO.File.WriteAllBytes(path, ProxyGui.Properties.Resources.ProxyLib);
+            this.Text = "Disconected";
+            this.TxtCientPort.Enabled = false;
+            this.TxtHostPort.Enabled = false;
+        }
+
+        private void BtnStartStop_Click(object sender, EventArgs e)
+        {
+            
+            if (((Button)sender).Text=="Start")
+            {   
+                _prox = new Proxy();
+                _prox.SessionStarted += _prox_SessionStarted;
+                _prox.SessionTerminated+=_prox_SessionTerminated;
+                _prox.setCientport(chkAdvanced.Checked? TxtCientPort.Text:"22").sethost(TxtHostName.Text).setpassword(TxtPassword.Text).setServerport(chkAdvanced.Checked?TxtHostPort.Text:"8080").setusername(TxtUserName.Text).Verbose(ChkVerbose.Checked);
+                _prox.SessionTerminated += _prox_SessionTerminated;
+                _prox.Start();
+                this.Text = "connecting";
+                button1.Text = "Stop";
+            }
+            else if (((Button)sender).Text == "Stop")
+            {
+                button1.Text = "Closing";
+                this.Text = "Disconecting";
+                _prox.Stop();
+                
+            }
+        }
+
+        void _prox_SessionStarted(object source, ProxyInfo e)
+        {
+            
+            SetControlPropertyThreadSafe(this, "Text", "Conected");
+        }
+
+        void _prox_SessionTerminated(object source, ProxyInfo e)
+        {
+            SetControlPropertyThreadSafe(this, "Text", "Disconected");
+            SetControlPropertyThreadSafe(button1, "Text", "Start");
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            
+            if (e.KeyCode==Keys.Enter)
+            BtnStartStop_Click(button1, new EventArgs());
+        }
+
+        private delegate void SetControlPropertyThreadSafeDelegate(Control control, string propertyName, object propertyValue);
+
+        public static void SetControlPropertyThreadSafe(Control control, string propertyName, object propertyValue)
+        {
+            if (control.InvokeRequired)
+            {
+                control.Invoke(new SetControlPropertyThreadSafeDelegate(SetControlPropertyThreadSafe), new object[] { control, propertyName, propertyValue });
+            }
+            else
+            {
+                control.GetType().InvokeMember(propertyName, BindingFlags.SetProperty, null, control, new object[] { propertyValue });
+            }
+        }
+
+        private void chkAdvanced_CheckedChanged(object sender, EventArgs e)
+        {
+            this.TxtCientPort.Enabled = chkAdvanced.Checked;
+            this.TxtHostPort.Enabled = chkAdvanced.Checked;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (button1.Text == "Stop")
+            {
+                button1.Text = "Closing";
+                this.Text = "Disconecting";
+                _prox.Stop();
+
+            }
+        }
+    }
+}
