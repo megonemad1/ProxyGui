ec26c729aec54ec3488578c8695f0de45e2c00a0
James W <jameswalsh5683@gmail.com>
2015-03-17 09:46:56 +0000

    shits bork somehow

--- ProxyLib/ProxyLib/Proxy.cs
+++ ProxyLib/ProxyLib/Proxy.cs
@@ -170,6 +170,7 @@ namespace ProxyLib
                 System.IO.File.WriteAllBytes(path, Properties.Resources.klink);
             ProcessStartInfo startInfo = new ProcessStartInfo();
             startInfo.FileName = path;
+            //-c for compression
             startInfo.Arguments = string.Format("-ssh -l {0} -pw {1} -D {2} -P {3} {4} {5} {6} {7}", username, password, this.Cientport, this.Serverport, (verbose) ? "-v" : "", (auto_store_sshkey) ? "-auto_store_sshkey" : "", (NoShell) ? "-N" : "", host);
             startInfo.UseShellExecute = false;
             startInfo.CreateNoWindow = NoShell;
@@ -198,7 +199,7 @@ namespace ProxyLib
             }
             catch (Exception e)
             {
-                Console.WriteLine("[Runtime]" + e.Message);
+                Console.WriteLine("[Runtime Error]" + e.Message);
             }
 
             _open = false;
